949ad15c8291bab260e03fc16bf623d7874cbbe4
Vadim <duf1lt@yandex.ru>
2021-11-25 08:54:32 +0300

    create calculator class

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public double Sum(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException($"Argument {nameof(b)} can't be zero!");
+
+            return a / b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Sqrt(double a)
+        {
+            if (a < 0)
+                throw new ArgumentException($"Argument {nameof(a)} can't be less then zero");
+
+            return Math.Sqrt(a);
+        }
+    }
+}
---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,28 @@
+﻿
+var calc = new Calculator.Calculator();
+double a = 8;
+double b = 16;
+
+Console.WriteLine(calc.Sum(a, b));
+Console.WriteLine(calc.Substract(a, b));
+Console.WriteLine(calc.Multiply(a, b));
+Console.WriteLine(calc.Divide(a, b));
+Console.WriteLine(calc.Sqrt(a));
+
+try
+{
+    Console.WriteLine(calc.Divide(a, 0));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+
+try 
+{
+    Console.WriteLine(calc.Sqrt(-5));
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
